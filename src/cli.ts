077c713ce6e3d03a3f8b3643bc766ee0d039ba73
Zongmin Lei <leizongmin@gmail.com>
2017-06-26 16:51:34 +0800

    优化界面显示

--- src/cli.ts
+++ src/cli.ts
@@ -10,10 +10,11 @@ import * as path from 'path';
 import * as yargs from 'yargs';
 import * as clc from 'cli-color';
 import * as yaml from 'js-yaml';
+import 'source-map-support/register';
 import HTTPProxy from './';
 const pkgInfo = require('../package');
 
-main();
+process.nextTick(main);
 
 interface Config {
   host: string;
@@ -37,25 +38,27 @@ function emptyLine(): void {
 }
 
 function pageLine(): void {
-  console.log('================================================================================================================================================================'
+  console.log('----------------------------------------------------------------------------------------------------------------------------------------------------------------'
     .slice(0, (process.stdout as any).columns));
 }
 
-function warn(...args: any[]): void {
-  console.error(clc.bgYellow.white(' warn  ') + '\t' + clc.yellow(util.format.call(util, ...args)));
-}
-
-function error(...args: any[]): void {
-  console.error(clc.bgRed.white(' error ') + '\t' + clc.red(util.format.call(util, ...args)));
-}
-
-function info(...args: any[]): void {
-  console.log(clc.bgBlue.white(' info  ') + '\t' + clc.blue(util.format.call(util, ...args)));
-}
-
-function debug(...args: any[]): void {
-  console.log(clc.bgGreen.white(' debug ') + '\t' + clc.green(util.format.call(util, ...args)));
-}
+const logger = {
+  warn(...args: any[]): void {
+    console.error(clc.bgYellow.white(' warn  ') + '\t' + clc.yellow(util.format.call(util, ...args)));
+  },
+  error(...args: any[]): void {
+    console.error(clc.bgRed.white(' error ') + '\t' + clc.red(util.format.call(util, ...args)));
+  },
+  info(...args: any[]): void {
+    console.log(clc.bgBlue.white(' info  ') + '\t' + clc.blue(util.format.call(util, ...args)));
+  },
+  debug(...args: any[]): void {
+    console.log(clc.bgGreen.white(' debug ') + '\t' + clc.green(util.format.call(util, ...args)));
+  },
+  trace(...args: any[]): void {
+    console.log(clc.bgGreen.white(' trace ') + '\t' + clc.green(util.format.call(util, ...args)));
+  }
+};
 
 function showWelcome(): void {
   pageLine();
@@ -68,11 +71,11 @@ function showWelcome(): void {
 
 function showBanner(): void {
   console.log(clc.cyan(`
-       __               __  __  __        __   __   __
- /    /    /      /  | /|  /|  /  |      /  | /  | /  | / /  /  |
-(    (___ (      (___|( | ( | (___|     (___|(___|(   |(_/_ (___|
-|   )|    |      |   )  |   | |         |    |\   |   ) /  )    )
-|__/ |__  |      |  /   |   | |         |    | \  |__/ /  /  __/
+         __               __  __  __        __   __   __
+   /    /    /      /  | /|  /|  /  |      /  | /  | /  | / /  /  |
+  (    (___ (      (___|( | ( | (___|     (___|(___|(   |(_/_ (___|
+  |   )|    |      |   )  |   | |         |    |\   |   ) /  )    )
+  |__/ |__  |      |  /   |   | |         |    | \  |__/ /  /  __/
 `));
 }
 
@@ -86,10 +89,10 @@ function showHelp(): void {
 }
 
 function loadConfig(configFile: string): Config {
-  info('读取配置文件: %s', configFile);
+  logger.info('读取配置文件: %s', configFile);
   const config: Config = yaml.safeLoad(fs.readFileSync(configFile).toString());
   if (!config) {
-    error('读取配置文件出错: %s', configFile);
+    logger.error('读取配置文件出错: %s', configFile);
   }
   if (!config.host) {
     config.host = '127.0.0.1';
@@ -107,51 +110,51 @@ function loadConfig(configFile: string): Config {
 
 function startProxy(configFile: string): void {
   if (!configFile) {
-    error('请指定配置文件！');
+    logger.error('请指定配置文件！');
     emptyLine();
     process.exit(1);
   }
   configFile = path.resolve(configFile);
   let config = loadConfig(configFile);
 
-  info('正在启动代理服务器...');
+  logger.info('正在启动代理服务器...');
   const proxy = new HTTPProxy();
   proxy.on('proxy', proxy => {
     if (proxy.rewrite) {
-      info('改写代理 %s %s => %s', proxy.method, proxy.origin, proxy.target);
+      logger.info('改写代理 %s %s => %s', proxy.method, proxy.origin, proxy.target);
     } else {
-      debug('直接代理 %s %s', proxy.method, proxy.origin);
+      logger.debug('直接代理 %s %s', proxy.method, proxy.origin);
     }
   });
   proxy.on('addRule', rule => {
-    info('增加代理规则: %s => %s', rule.match, rule.proxy);
+    logger.info('增加代理规则: %s => %s', rule.match, rule.proxy);
   });
   proxy.on('removeRule', rule => {
-    info('删除代理规则: %s => %s', rule.match, rule.proxy);
+    logger.info('删除代理规则: %s => %s', rule.match, rule.proxy);
   });
   proxy.on('error', err => {
-    error('%s', err.stack);
+    logger.error('%s', err.stack);
   });
 
   // 加载规则到proxy
   const setConfigToProxy = () => {
     if (config.debug) {
-      info('打开调试输出');
-      proxy.debugHandler = debug;
+      logger.info('打开调试输出');
+      proxy.debugHandler = logger.trace;
     } else {
-      info('关闭调试输出');
+      logger.info('关闭调试输出');
       proxy.debugHandler = () => {};
     }
     proxy.removeAllRules();
     config.rules.forEach((rule, i) => {
       if (!(rule.match && typeof rule.match === 'string')) {
-        return warn('第%s个代理配置格式不正确: 缺少match参数: %s', i, rule.match);
+        return logger.warn('第%s个代理配置格式不正确: 缺少match参数: %s', i, rule.match);
       }
       if (!(rule.proxy && typeof rule.proxy === 'string')) {
-        return warn('第%s个代理配置格式不正确: 缺少proxy参数: %s', i, rule.proxy);
+        return logger.warn('第%s个代理配置格式不正确: 缺少proxy参数: %s', i, rule.proxy);
       }
       if (rule.match.indexOf('http://') !== 0) {
-        return warn('第%s个代理配置格式不正确: 只支持更改http协议的请求: %s', i, rule.match);
+        return logger.warn('第%s个代理配置格式不正确: 只支持更改http协议的请求: %s', i, rule.match);
       }
       proxy.addRule(rule);
     });
@@ -163,7 +166,7 @@ function startProxy(configFile: string): void {
   let reloadConfigTid: NodeJS.Timer;
   const reloadConfig = () => {
     const delay = 2;
-    debug('%s秒后重载配置...', delay);
+    logger.debug('%s秒后重载配置...', delay);
     clearTimeout(reloadConfigTid);
     reloadConfigTid = setTimeout(() => {
       config = loadConfig(configFile);
@@ -172,14 +175,14 @@ function startProxy(configFile: string): void {
   }
 
   proxy.server.listen(config.port, config.host, () => {
-    info('服务器已启动');
-    info('请设置代理服务器为 http://%s:%s', config.host === '0.0.0.0' ? '127.0.0.1' : config.host, config.port);
+    logger.info('服务器已启动');
+    logger.info('请设置代理服务器为 http://%s:%s', config.host === '0.0.0.0' ? '127.0.0.1' : config.host, config.port);
   });
 
   // 重载配置
   fs.watch(configFile, (event, filename) => {
     if (event === 'change') {
-      info('配置文件已改变: %s %s', event, filename);
+      logger.info('配置文件已改变: %s %s', event, filename);
       reloadConfig();
     }
   });
@@ -202,12 +205,12 @@ function main(): void {
     default:
       showWelcome();
       if (cmd) {
-        warn('不支持命令"%s"', cmd);
+        logger.warn('不支持命令"%s"', cmd);
         emptyLine();
       }
       showHelp();
   }
 }
 
-process.on('uncaughtException', err => error(err.stack));
-process.on('unhandledRejection', err => error(err.stack));
+process.on('uncaughtException', err => logger.error(err.stack));
+process.on('unhandledRejection', err => logger.error(err.stack));
